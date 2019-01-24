3861778a2e212327d43d5a847331bf5ad83744a0
SeoHyeIn <bogo96@kaist.ac.kr>
2019-01-24 18:02:35 +0900

    Check login

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,12 +3,22 @@ const {caver, incidents, incident} = require('../models/caver');
 
 exports.login = function(req, res) {
 
-    models.Login.create({
-        userId: req.body['userId'], 
-        expotoken: req.body['expotoken'], 
-        isAdmin: req.body['isAdmin']
-    })
-    .then((result) => { res.json(result); })
-    .catch((error) => { console.log(error); });
+    if (isRegistration) {
+        var user = JSON.parse(JSON.stringify(isRegistration));
+        if (user['userId'] === req.body['userId'] && user['isAdmin'] === req.body['isAdmin']) {
+            res.json(user);
+        } else {
+            res.status(401).send();
+        }
+    } else {
+        models.Login.create({
+            userId: req.body['userId'], 
+            expotoken: req.body['expotoken'], 
+            isAdmin: req.body['isAdmin']
+        })
+        .then((result) => { res.json(result); })
+        .catch((error) => { console.log(error); });
+    }
+;
 
 } 
