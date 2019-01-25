5acc3e4c582da012b8fca89b8b0f8f3887bb5e8a
hyperthreading <no1expman@gmail.com>
2019-01-25 16:59:18 +0900

    Add code to start with HTTPS

--- server/app.js
+++ server/app.js
@@ -23,6 +23,22 @@ var server = app.listen(port, function(){
 	console.log("Server has started on port [" + port + "]");
 });
 
+const enableHTTPS = process.env.ENABLE_HTTPS;
+
+if (enableHTTPS) {
+  const https = require('https');
+  const fs = require('fs');
+
+  const port = process.env.HTTPS_PORT | 443;
+  const options = {
+    key: fs.readFileSync('https/key.pem'),
+    cert: fs.readFileSync('https/cert.pem')
+  };
+  
+  https.createServer(options, app).listen(port, function() {
+    console.log(`HTTPS has started on port [${port}]`)
+  });
+}
 
 
 
