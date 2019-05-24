a6f6afcb3c991a98c789856069385326e3895d09
SeoHyeIn <bogo96@kaist.ac.kr>
2019-05-24 22:31:09 +0900

    Fix login null error

--- server/routes/user.js
+++ server/routes/user.js
@@ -68,49 +68,57 @@ exports.profile = function(req, res) {
 
 // SSO 토큰을 확인하고 서버 jwt 토큰을 발급한다
 passport.use(new BearerStrategy(
-    (token, done) => {
+    async (token, done) => {
 
         const url = 'https://iam.kaist.ac.kr:443/iamps/services/appsingleauth?wsdl';
         const privkey = process.env.PRIVKEY;
         const args = {cookieValue: token, PrivateKeyStr: privkey, adminVO: {adminId: null, password: null}};
-        soap.createClientAsync(url)
-        .then(client => { 
-            client.AppSinglAuthApiService.AppSinglAuthApiPort.verification(args, async function(err, result) {
-                if (err) {
-                    return done(err);
-                }
-                if (err===null && result.return===null) {
-                    return done( new Error('SSO return null'));
-                }
+        var isAdmin = false;
+        var retry = 5;
+        var info = null;
 
-                //TODO : isAdmin 확인하기 (안전팀 부서코드)
-                var isAdmin = false;
-                if (result.return.ku_kaist_org_id === '3502') {
-                    isAdmin = true;
-                }
 
-                const user = await models.Users.findOrCreate({
-                    where: {kaist_uid: result.return.kaist_uid}, 
-                    defaults: {
-                        displayname: result.return.displayname, 
-                        ku_kname: result.return.ku_kname,
-                        ku_kaist_org_id: result.return.ku_kaist_org_id,
-                        mobile: result.return.mobile,
-                        isAdmin: isAdmin,
-                    }
+        try {
+            var client = await soap.createClientAsync(url);
+            var verification = new Promise (function( resolve, reject) {
+                client.AppSinglAuthApiService.AppSinglAuthApiPort.verification(args, async function(err, result) {
+                    if (err) reject (result.result);
+                    else resolve (result.return);
                 });
-             
-                const appToken = jwt.sign(
-                    { id: user[0]['id'] },
-                    secret,
-                    { expiresIn }
-                );
+            });
+
+            while (retry) {
+                info = await verification;   
+                if (info == null) retry--;
+                else retry = 0;
+            }
+                        
+            if (info == null) done(new Error('SSO return null'));
+
+            if (info.ku_kaist_org_id === '3502') isAdmin = true;
+            const user = await models.Users.findOrCreate({
+                where: {kaist_uid: info.kaist_uid}, 
+                defaults: {
+                    displayname: info.displayname, 
+                    ku_kname: info.ku_kname,
+                    ku_kaist_org_id: info.ku_kaist_org_id,
+                    mobile: info.mobile,
+                    isAdmin: isAdmin,
+                }
+            });
         
-                done(null, { appToken: appToken, id: user[0]['id']});
-            
-            }); 
-        })
-        .catch((error)=>{ done(error); });
+            const appToken = jwt.sign(
+                { id: user[0]['id'] },
+                secret,
+                { expiresIn }
+            );
+        
+            done(null, { appToken: appToken, id: user[0]['id']});
+
+        } catch (e) {
+            console.log(e);
+            return done(new Error("JWT token fail."));
+        }
     }
 ));
 
