a9b9f211025d4aa7c4eee159962d6ca075e32ce0
SeoHyeIn <bogo96@kaist.ac.kr>
2019-10-08 22:25:53 +0900

    Move migration file

--- server/migrations/20191008120804-AddTrainingMode.js
+++ server/migrations/20191008120804-AddTrainingMode.js
@@ -2,16 +2,17 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return [
-      queryInterface.addColumn('Users', 'isTraining', {
+    return queryInterface.addColumn('Users', 'isTraining', {
         type: Sequelize.BOOLEAN, 
         defaultValue: false
-      }),
-      queryInterface.addColumn('Incidents', 'isTraining', {
-        type: Sequelize.BOOLEAN, 
-        defaultValue: false
+      }).then(()=>{
+        return queryInterface.addColumn('Incidents', 'isTraining', {
+          type: Sequelize.BOOLEAN, 
+          defaultValue: false
+        })
       })
-    ];
+      
+
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -22,10 +23,10 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return [
-      queryInterface.removeColumn('Users', 'isTraining'),
-      queryInterface.removeColumn('Incidents', 'isTraining')
-    ];
+    return queryInterface.removeColumn('Users', 'isTraining').then(() => {
+      return queryInterface.removeColumn('Incidents', 'isTraining')
+    })
+      
     
     /*
       Add reverting commands here.
