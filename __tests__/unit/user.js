ca1745533fbe3df89cf4b4264ff1c9410b51b0a4
hyperthreading <no1expman@gmail.com>
2019-11-21 00:30:51 +0900

    Refactor test to be more self contained

--- __tests__/unit/user.js
+++ __tests__/unit/user.js
@@ -1,3 +1,5 @@
+process.env.SECRET = "1230";
+
 const { issueTokenWith } = require("../../server/routes/user");
 const jwt = require("jsonwebtoken");
 jest.mock('../../server/utils/Log');
@@ -13,21 +15,24 @@ describe('issueTokenWith', () => {
    });
 
   it('should hand over sso result to the createUser function', done => {
+    const ssoService = () => ({ department: "somewhere" })
     const createUser = (result) => {
       expect(result.department).toBe("somewhere");
       return [{ id: "123" }];
     };
     const authDone = (err, response) => {
+      expect(err).toBeFalsy();
+      expect(response).toBeDefined();
       done();
     };
-    issueTokenWith(() => { department: "somewhere" }, createUser)("", authDone);
+    issueTokenWith(ssoService, createUser)("", authDone);
   });
 
   it('should return jwt token with id of the created record', done => {
     const authDone = (err, response) => {
       expect(err).toBeFalsy();
       // TODO:: Verify and validate response.appToken
-      // console.log("verify", jwt.verify(response.appToken, "123"));
+      console.log("verify", jwt.verify(response.appToken, "1230"));
       done();
     };
     const createUser = () => [{ id: "123" }];
